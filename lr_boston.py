919a6a1aba4c532a33e53c268505a13fea42fbbc
kevguy <kevlai22@uw.edu>
2016-02-27 10:40:40 +0800

    Add code to find best fitted feature name and score in Boston dataset

--- lr_boston.py
+++ lr_boston.py
@@ -11,28 +11,36 @@ print 'Number of samples in the Boston dataset is: ', len(boston['data'])
 ### The following line gives the same answer
 ### print 'Number of samples in the Boston dataset is: ', len(boston['target'])
 
-# which feature
-i_feature = 0
-# Get the feature name
-feature_name = boston.feature_names[i_feature]
-print 'Feature name is', feature_name
+best_score = -1000
+for i_feature in range(0, len(boston['feature_names'])):
+	# Get the feature name
+	feature_name = boston.feature_names[i_feature]
+	print 'Feature name is', feature_name
 
-# Use only one feature
-diabetes_X = boston.data[:, np.newaxis, i_feature]
+	# Use only one feature
+	diabetes_X = boston.data[:, np.newaxis, i_feature]
 
-# Split the data into training/testing sets
-boston_X_train = diabetes_X[:-20]
-boston_X_test = diabetes_X[-20:]
+	# Split the data into training/testing sets
+	boston_X_train = diabetes_X[:-20]
+	boston_X_test = diabetes_X[-20:]
 
-# Split the targets into training/testing sets
-boston_y_train = boston.target[:-20]
-boston_y_test = boston.target[-20:]
+	# Split the targets into training/testing sets
+	boston_y_train = boston.target[:-20]
+	boston_y_test = boston.target[-20:]
 
-# Create linear regression object
-model = linear_model.LinearRegression()
+	# Create linear regression object
+	model = linear_model.LinearRegression()
 
-# Train the model using the training sets
-model.fit(boston_X_train, boston_y_train)
+	# Train the model using the training sets
+	model.fit(boston_X_train, boston_y_train)
 
-# Explained variance score: score=1 is perfect prediction
-model_score = model.score(boston_X_test, boston_y_test)
+	# Explained variance score: score=1 is perfect prediction
+	model_score = model.score(boston_X_test, boston_y_test)
+	print 'The score is ', model_score, '\n'
+
+	if model_score > best_score:
+		best_score = model_score
+		best_feature = feature_name
+
+print 'Best fitted feature name is: ', best_feature
+print 'Best fitted model score is: ', best_score

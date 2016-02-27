603aa95ecf1d259c8f1806ac1d441cb9c343e1f7
kevguy <kevlai@uw.edu>
2016-02-27 18:11:29 +0800

    Add code to import raw data

--- lr_imports85.py
+++ lr_imports85.py
@@ -5,9 +5,14 @@ from sklearn import datasets, linear_model
 import seaborn
 seaborn.set()
 
-# df = pd.read_csv('imports-85.data',
-#                  header=None,
-#                  names=[#Give exact names to columns according to imports-85.names#],
-#                      na_values=(#Some values are missing, treat the '?' with NaN#))
+df = pd.read_csv('imports-85.data',
+                  header=None,
+                  names=['symboling', 'normalized-losses', 'make', 'fuel-type', 'aspiration', 'num-of-doors', 'body-style', 'drive-wheels',
+                         'engine-location', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'engine-type', 'num-of-cylinders',
+                         'engine-size', 'fuel-system', 'bore', 'stroke', 'compression-ratio',
+                         'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg', 'price'],
+                      na_values='?')
 
-
+print df
+df = df.dropna()
+print df

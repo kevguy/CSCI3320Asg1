f2ac4a1124455b983e1066eb05734a367748dc12
kevguy <kevlai@uw.edu>
2016-03-09 23:30:58 +0800

    feat: Add code for logistic regression

--- simple_clf.py
+++ simple_clf.py
@@ -17,3 +17,34 @@ y[n_samples // 2:] = 1
 X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=42)
 log_reg = linear_model.LogisticRegression()
 
+
+# Train the model using the training sets
+log_reg.fit(X_train, y_train)
+
+# Prediction
+pred = log_reg.predict(X_test)
+print 'prediction', pred
+
+### draw the scatterplot, with color-coded training and testing points
+class_0_color = "b"
+class_1_color = "r"
+import matplotlib.pyplot as plt
+for test, prediction in zip(X_test, pred):
+	if (prediction == 0):
+		plt.scatter( test[0], test[1], color=class_0_color ) 
+	else:
+		plt.scatter( test[0], test[1], color=class_1_color ) 
+
+plt.title('Logistic Regression Prediction')
+plt.show()
+
+correct = 0
+count = 0
+for y_test, prediction in zip(y_test, pred):
+	if (prediction == y_test):
+		correct = correct + 1
+	count = count + 1
+
+print 'Number of sammples in testing set: ', count
+print 'Number of correct predictions: ', correct
+print 'Number of wrong predictions: ', count - correct

c89c0d12670aab647e6fd37cddf741b8b4258bab
kevguy <kevlai@uw.edu>
2016-03-09 22:49:04 +0800

    feat: Add code for polynomial regression

--- lr_regular.py
+++ lr_regular.py
@@ -10,6 +10,8 @@ y_train = [[7.5], [9.1], [13.2], [17.5], [19.3]]
 X_test = [[6], [8], [11], [16]]
 y_test = [[8.3], [12.5], [15.4], [18.6]]
 
+
+### LR Regression on polynomial data
 lr_model = LinearRegression()
 lr_model.fit(X_train, y_train)
 xx = np.linspace(0, 26, 100)
@@ -23,11 +25,26 @@ print 'intercept: ', lr_model.intercept_
 plt.show()
 
 
-# poly = PolynomialFeatures(degree=5)
-# X_train_poly = poly.fit_transform(X_train)
-# X_test_poly = poly.transform(X_test)
-#
-#
+### Polynomial regression on training data
+poly = PolynomialFeatures(degree=5)
+X_train_poly = poly.fit_transform(X_train)
+X_test_poly = poly.transform(X_test)
+pr_model = LinearRegression()
+pr_model.fit(X_train_poly, y_train)
+print 'powers:', poly.powers_
+print 'coefficients: ', pr_model.coef_
+print 'intercept: ', pr_model.intercept_
+xx_poly = poly.transform(xx.reshape(xx.shape[0], 1))
+yy_poly = pr_model.predict(xx_poly)
+### score = reg.score(feature_test.reshape(-1, 1), target_test.reshape(-1,1))
+pr_score = pr_model.score(X_test_poly, y_test)
+print 'score: ', pr_score
+plt.plot(xx_poly, yy_poly)
+plt.plot(X_test_poly, y_test)
+plt.title('Linear regression (order 5) result')
+plt.show()
+
+
 # ridge_model = Ridge(alpha=4, normalize=False)
 # ridge_model.fit(X_train_poly, y_train)
 
